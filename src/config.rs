d27712647d7d57638782f6b2e5b11d14e55bdb48
Anurag Singh <contact@anurag.sh>
2023-04-14 21:53:45 +0800

    thats a neat trick

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,13 @@
 use crate::types::ApplicationConfig;
 use config::{Config, Environment};
-use foundation::{
-    aws,
-    config::sources::{secret_manager::SecretsManagerSource, shared_secrets::SharedSecretsSource},
-};
+use foundation::{aws, config::sources::secret_manager::SecretsManagerSource};
 
 pub async fn get_application_config() -> Result<ApplicationConfig, anyhow::Error> {
     let shared_config = aws::config::get_shared_config().await;
 
     let secrets = aws_sdk_secretsmanager::Client::new(&shared_config);
     let secret_manager_source = SecretsManagerSource::new("Ozb-", secrets.clone());
-    let shared_secrets_source = SharedSecretsSource::new(secrets);
+    let shared_secrets_source = SecretsManagerSource::new("Shared-", secrets);
 
     Ok(Config::builder()
         .add_async_source(secret_manager_source)
