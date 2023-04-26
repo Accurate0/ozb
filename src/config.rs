e05d0c030388fa494c617795f45e4bb76d10ffe2
Anurag Singh <contact@anurag.sh>
2023-04-26 20:09:07 +0800

    not required

--- src/config.rs
+++ src/config.rs
@@ -7,7 +7,7 @@ pub async fn get_application_config() -> Result<ApplicationConfig, anyhow::Error
 
     let secrets = aws_sdk_secretsmanager::Client::new(&shared_config);
     let secret_manager_source = SecretsManagerSource::new("Ozb-", secrets.clone());
-    let shared_secrets_source = SecretsManagerSource::new("Shared-", secrets);
+    let shared_secrets_source = SecretsManagerSource::new("Shared-", secrets).with_required(false);
 
     Ok(Config::builder()
         .add_async_source(secret_manager_source)
