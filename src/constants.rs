3bdf2695bc7a8e02da842353a757f59453ec0ad0
Anurag Singh <contact@anurag.sh>
2024-04-25 12:47:14 +0800

    change checkin url

--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,9 @@
 pub mod cfg {
     pub const OZB_RSS_DEALS_URL: &str = "https://www.ozbargain.com.au/deals/feed";
     pub const UPTIME_DAEMON_PUSH_URL: &str =
-        "https://uptime.anurag.sh/api/push/UbclEdv2wQ?status=up&msg=OK&ping=";
+        "https://uptime.anurag.sh/checkin/Qo2HHYuzUIzH0SfUCdRnoWjQ2qBhbhye";
     pub const UPTIME_BOT_PUSH_URL: &str =
-        "https://uptime.anurag.sh/api/push/rE5Ori1htI?status=up&msg=OK&ping=";
+        "https://uptime.anurag.sh/checkin/hackBHlWLM4MSG3wocmnSg3EJGpNIW0b";
     #[cfg(debug_assertions)]
     pub const REDIS_KEY_PREFIX: &str = "OZB_DEBUG";
     #[cfg(not(debug_assertions))]
