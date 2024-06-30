e0caca5bd9d7d1673d749a1007baf5e605b20e5a
Anurag Singh <contact@anurag.sh>
2024-06-30 20:53:24 +0800

    remove

--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,5 @@
 pub mod cfg {
     pub const OZB_RSS_DEALS_URL: &str = "https://www.ozbargain.com.au/deals/feed";
-    pub const UPTIME_DAEMON_PUSH_URL: &str =
-        "https://uptime.anurag.sh/checkin/Qo2HHYuzUIzH0SfUCdRnoWjQ2qBhbhye";
-    pub const UPTIME_BOT_PUSH_URL: &str =
-        "https://uptime.anurag.sh/checkin/ygdWEnzCDcDQ8opvzOPrpiUkXzDHfoOD";
     #[cfg(debug_assertions)]
     pub const REDIS_KEY_PREFIX: &str = "OZB_DEBUG";
     #[cfg(not(debug_assertions))]
