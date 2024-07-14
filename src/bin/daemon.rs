50ae28b97c6055dea743cebf97152799174a8e39
Anurag Singh <contact@anurag.sh>
2024-07-14 20:04:29 +0800

    update tracing

--- src/bin/daemon.rs
+++ src/bin/daemon.rs
@@ -12,6 +12,7 @@ use ozb::tracing::init_logger;
 use ozb::{skip_option, skip_result};
 use redis::AsyncCommands;
 use serde_json::Value;
+use tracing::{Instrument, Level};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -23,123 +24,126 @@ async fn main() -> Result<(), Error> {
     let redis = &client.get_connection_manager().await.ok();
     let key = &format!("{}_{}", REDIS_KEY_PREFIX, "ETAG");
 
-    lambda_runtime::run(service_fn(move |_: LambdaEvent<Value>| async move {
-        let redis = redis.clone();
-        let etag: Option<String> = match redis.clone() {
-            Some(mut redis) => match redis.get(key).await {
-                Ok(value) => value,
-                Err(e) => {
-                    tracing::error!("error getting redis key: {}", e);
-                    None
-                }
-            },
-            None => None,
-        };
-
-        let response = http_client
-            .get(OZB_RSS_DEALS_URL)
-            .header(IF_NONE_MATCH, etag.unwrap_or_default())
-            .send()
-            .await?;
-
-        if response.status() == StatusCode::NOT_MODIFIED {
-            tracing::info!("304 response, skipping...");
-            return Ok(());
-        } else {
-            let resp_headers = response.headers().clone();
-            // update etag when content changed
-            // set etag
-            let etag = resp_headers
-                .get(ETAG)
-                .map(|h| h.to_str().unwrap_or_default().to_owned());
-
-            if let Some(etag) = etag {
-                if let Some(mut redis) = redis {
-                    match redis.set::<_, _, ()>(key, etag).await {
-                        Ok(_) => {}
-                        Err(e) => tracing::error!("error setting redis key: {}", e),
-                    };
-                }
-            };
-        }
-
-        let response = response.bytes().await?;
-        let channel = rss::Channel::read_from(&response[..])?;
-
-        for item in channel.items() {
-            let guid = skip_option!(item.guid(), "guid")
-                .value()
-                .split_ascii_whitespace()
-                .next();
-
-            let guid = skip_option!(guid, "guid whitespace").to_owned();
-
-            let title = skip_option!(item.title(), "title").to_owned();
-            // Sun, 26 Mar 2023 17:29:29 +1100
-            let publication_date = skip_result!(
-                DateTime::parse_from_str(
-                    skip_option!(item.pub_date(), "publication date"),
-                    "%a, %d %b %Y %T %z",
-                ),
-                "publication date"
-            );
-            let link = skip_option!(item.link(), "link").to_owned();
-            let description = skip_option!(item.description(), "description").to_owned();
-            let ext = item.extensions();
-            let thumbnail = || -> Result<String, anyhow::Error> {
-                Ok(ext
-                    .get("media")
-                    .context("must have thumbnail")?
-                    .get("thumbnail")
-                    .context("must have url 1")?
-                    .first()
-                    .context("must have url 2")?
-                    .attrs
-                    .get("url")
-                    .context("must have url 3")?
-                    .to_owned())
+    lambda_runtime::run(service_fn(move |_: LambdaEvent<Value>| {
+        async move {
+            let redis = redis.clone();
+            let etag: Option<String> = match redis.clone() {
+                Some(mut redis) => match redis.get(key).await {
+                    Ok(value) => value,
+                    Err(e) => {
+                        tracing::error!("error getting redis key: {}", e);
+                        None
+                    }
+                },
+                None => None,
             };
 
-            let categories: Vec<_> = item
-                .categories()
-                .iter()
-                .map(|c| c.name.to_owned().replace("&amp;", "&"))
-                .collect();
+            let response = http_client
+                .get(OZB_RSS_DEALS_URL)
+                .header(IF_NONE_MATCH, etag.unwrap_or_default())
+                .send()
+                .await?;
 
-            tracing::info!("inserting: {} - {} - {}", guid, title, link);
-            let added = prisma_client
-                .posts()
-                .upsert(
-                    posts::UniqueWhereParam::OzbIdEquals(guid.clone()),
-                    (
-                        guid,
-                        title,
-                        publication_date,
-                        link,
-                        description,
-                        vec![
-                            posts::thumbnail::set(thumbnail().ok()),
-                            posts::categories::set(categories.clone()),
-                        ],
+            if response.status() == StatusCode::NOT_MODIFIED {
+                tracing::info!("304 response, skipping...");
+                return Ok(());
+            } else {
+                let resp_headers = response.headers().clone();
+                // update etag when content changed
+                // set etag
+                let etag = resp_headers
+                    .get(ETAG)
+                    .map(|h| h.to_str().unwrap_or_default().to_owned());
+
+                if let Some(etag) = etag {
+                    if let Some(mut redis) = redis {
+                        match redis.set::<_, _, ()>(key, etag).await {
+                            Ok(_) => {}
+                            Err(e) => tracing::error!("error setting redis key: {}", e),
+                        };
+                    }
+                };
+            }
+
+            let response = response.bytes().await?;
+            let channel = rss::Channel::read_from(&response[..])?;
+
+            for item in channel.items() {
+                let guid = skip_option!(item.guid(), "guid")
+                    .value()
+                    .split_ascii_whitespace()
+                    .next();
+
+                let guid = skip_option!(guid, "guid whitespace").to_owned();
+
+                let title = skip_option!(item.title(), "title").to_owned();
+                // Sun, 26 Mar 2023 17:29:29 +1100
+                let publication_date = skip_result!(
+                    DateTime::parse_from_str(
+                        skip_option!(item.pub_date(), "publication date"),
+                        "%a, %d %b %Y %T %z",
                     ),
-                    vec![],
-                )
-                .exec()
-                .await?;
+                    "publication date"
+                );
+                let link = skip_option!(item.link(), "link").to_owned();
+                let description = skip_option!(item.description(), "description").to_owned();
+                let ext = item.extensions();
+                let thumbnail = || -> Result<String, anyhow::Error> {
+                    Ok(ext
+                        .get("media")
+                        .context("must have thumbnail")?
+                        .get("thumbnail")
+                        .context("must have url 1")?
+                        .first()
+                        .context("must have url 2")?
+                        .attrs
+                        .get("url")
+                        .context("must have url 3")?
+                        .to_owned())
+                };
 
-            // this is the actual trigger
-            prisma_client
-                .trigger_ids()
-                .upsert(
-                    trigger_ids::UniqueWhereParam::PostIdEquals(added.id.clone()),
-                    (added.id, vec![]),
-                    vec![],
-                )
-                .exec()
-                .await?;
+                let categories: Vec<_> = item
+                    .categories()
+                    .iter()
+                    .map(|c| c.name.to_owned().replace("&amp;", "&"))
+                    .collect();
+
+                tracing::info!("inserting: {} - {} - {}", guid, title, link);
+                let added = prisma_client
+                    .posts()
+                    .upsert(
+                        posts::UniqueWhereParam::OzbIdEquals(guid.clone()),
+                        (
+                            guid,
+                            title,
+                            publication_date,
+                            link,
+                            description,
+                            vec![
+                                posts::thumbnail::set(thumbnail().ok()),
+                                posts::categories::set(categories.clone()),
+                            ],
+                        ),
+                        vec![],
+                    )
+                    .exec()
+                    .await?;
+
+                // this is the actual trigger
+                prisma_client
+                    .trigger_ids()
+                    .upsert(
+                        trigger_ids::UniqueWhereParam::PostIdEquals(added.id.clone()),
+                        (added.id, vec![]),
+                        vec![],
+                    )
+                    .exec()
+                    .await?;
+            }
+
+            Ok::<(), Error>(())
         }
-
-        Ok::<(), Error>(())
+        .instrument(tracing::span!(Level::INFO, "checking ozbargain"))
     }))
     .await?;
 
--- src/http.rs
+++ src/http.rs
@@ -1,9 +1,39 @@
 use anyhow::Context;
-use reqwest::Proxy;
+use reqwest::{Proxy, Request, Response};
 use reqwest_retry::policies::ExponentialBackoff;
 use reqwest_retry::RetryTransientMiddleware;
-use reqwest_tracing::TracingMiddleware;
-use std::time::Duration;
+use reqwest_tracing::{
+    default_on_request_end, reqwest_otel_span, ReqwestOtelSpanBackend, TracingMiddleware,
+};
+use std::time::{Duration, Instant};
+use tracing::Span;
+
+pub struct TimeTrace;
+impl ReqwestOtelSpanBackend for TimeTrace {
+    fn on_request_start(req: &Request, extension: &mut http::Extensions) -> Span {
+        let url = req.url().as_str();
+        extension.insert(Instant::now());
+
+        reqwest_otel_span!(
+            name = format!("{} {}", req.method(), url),
+            req,
+            url = url,
+            time_elapsed = tracing::field::Empty,
+            time_elapsed_formatted = tracing::field::Empty
+        )
+    }
+
+    fn on_request_end(
+        span: &Span,
+        outcome: &reqwest_middleware::Result<Response>,
+        extension: &mut http::Extensions,
+    ) {
+        let time_elapsed = extension.get::<Instant>().unwrap().elapsed().as_millis() as i64;
+        default_on_request_end(span, outcome);
+        span.record("time_elapsed", time_elapsed);
+        span.record("time_elapsed_formatted", format!("{time_elapsed}ms"));
+    }
+}
 
 fn get_default_middleware_http_client(
     client: reqwest::Client,
@@ -15,7 +45,7 @@ pub fn get_default_middleware(client: reqwest::Client) -> reqwest_middleware::Cl
     let retry_policy = ExponentialBackoff::builder().build_with_max_retries(2);
     reqwest_middleware::ClientBuilder::new(client)
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
-        .with(TracingMiddleware::default())
+        .with(TracingMiddleware::<TimeTrace>::new())
 }
 
 pub fn get_default_http_client() -> reqwest_middleware::ClientWithMiddleware {
