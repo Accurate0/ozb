50cd0bc15bc1fffd6fff8c20efdd22b419e70afc
Anurag Singh <contact@anurag.sh>
2024-07-15 18:54:05 +0800

    Revert "simple span export"
    This reverts commit 89571230c017410723f2bebf90d4433ecca7fba9.

--- src/bin/timed.rs
+++ src/bin/timed.rs
@@ -11,7 +11,6 @@ use ozb::{
     tracing::init_logger,
 };
 use serde_json::Value;
-use tracing::{Instrument, Level};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -19,41 +18,38 @@ async fn main() -> Result<(), Error> {
     let config = get_application_config().await?;
     let prisma_client = &prisma::new_client_with_url(&config.mongodb_connection_string).await?;
 
-    lambda_runtime::run(service_fn(move |_: LambdaEvent<Value>| {
-        async move {
-            // keep last 2 weeks of content, anything less than should be removed
-            // matches how long logs are kept
-            let datetime_2_weeks_ago = chrono::Utc::now()
-                .checked_sub_days(Days::new(14))
-                .ok_or(anyhow::Error::msg("Day not in range?"))?;
+    lambda_runtime::run(service_fn(move |_: LambdaEvent<Value>| async move {
+        // keep last 2 weeks of content, anything less than should be removed
+        // matches how long logs are kept
+        let datetime_2_weeks_ago = chrono::Utc::now()
+            .checked_sub_days(Days::new(14))
+            .ok_or(anyhow::Error::msg("Day not in range?"))?;
 
-            let deleted_posts = prisma_client
-                .posts()
-                .delete_many(vec![posts::WhereParam::AddedAt(DateTimeFilter::Lt(
-                    datetime_2_weeks_ago.into(),
-                ))])
-                .exec()
-                .await?;
+        let deleted_posts = prisma_client
+            .posts()
+            .delete_many(vec![posts::WhereParam::AddedAt(DateTimeFilter::Lt(
+                datetime_2_weeks_ago.into(),
+            ))])
+            .exec()
+            .await?;
 
-            let deleted_trigger_ids = prisma_client
-                .trigger_ids()
-                .delete_many(vec![trigger_ids::WhereParam::AddedAt(
-                    DateTimeNullableFilter::Lt(datetime_2_weeks_ago.into()),
-                )])
-                .exec()
-                .await?;
+        let deleted_trigger_ids = prisma_client
+            .trigger_ids()
+            .delete_many(vec![trigger_ids::WhereParam::AddedAt(
+                DateTimeNullableFilter::Lt(datetime_2_weeks_ago.into()),
+            )])
+            .exec()
+            .await?;
 
-            // TODO: audit entries export off mongo, to S3 or dynamo?
+        // TODO: audit entries export off mongo, to S3 or dynamo?
 
-            tracing::info!(
-                "deleted {} posts, {} trigger ids",
-                deleted_posts,
-                deleted_trigger_ids
-            );
+        tracing::info!(
+            "deleted {} posts, {} trigger ids",
+            deleted_posts,
+            deleted_trigger_ids
+        );
 
-            Ok::<(), Error>(())
-        }
-        .instrument(tracing::span!(parent: None, Level::INFO, "ozb::cleanup"))
+        Ok::<(), Error>(())
     }))
     .await?;
     Ok(())
